cb6b3e4baedac9805eaaba7940dc15aa819ccf11
tarasovc47 <tarasovc47@live.ru>
2016-12-03 22:21:15 +0500

    К wiki_esia подключен и налажен jQuery

--- js/wiki_esia.js
+++ js/wiki_esia.js
@@ -1,5 +1,78 @@
-var status = 0;
-function show_how(num) {
+var status=0;
+function show_how(){
+    $(".hidden.how").css("display","block");
+    $(".hidden.navigation").css("display","none");
+    $(".main.how").css("background","#f7605b");
+    $(".main.navigation").css("background","rgba(108, 107, 198, 0.3)");
+};
+function show_nav(){
+    $(".hidden.navigation").css("display","block");
+    $(".hidden.how").css("display","none");
+    $(".main.navigation").css("background","#f7605b");
+    $(".main.how").css("background","rgba(108, 107, 198, 0.3)");
+}
+function first_arrows_rotate(){
+    if(status==0) {
+        $(".more_info:eq(0)").css("display","block");
+        $(".arrow_down:eq(0)").css("transform","rotate(-180deg)");
+        status=1;
+    }
+    else if (status==1) {
+        $(".more_info:eq(0)").css("display","none");
+        $(".arrow_down:eq(0)").css("transform","rotate(-360deg)");
+        status=0;
+    }
+}
+function second_arrows_rotate(){
+    if(status==0) {
+        $(".more_info:eq(1)").css("display","block");
+        $(".arrow_down:eq(1)").css("transform","rotate(-180deg)");
+        status=1;
+    }
+    else if (status==1) {
+        $(".more_info:eq(1)").css("display","none");
+        $(".arrow_down:eq(1)").css("transform","rotate(-360deg)");
+        status=0;
+    }
+}
+function third_arrows_rotate(){
+    if(status==0) {
+        $(".more_info:eq(2)").css("display","block");
+        $(".arrow_down:eq(2)").css("transform","rotate(-180deg)");
+        status=1;
+    }
+    else if (status==1) {
+        $(".more_info:eq(2)").css("display","none");
+        $(".arrow_down:eq(2)").css("transform","rotate(-360deg)");
+        status=0;
+    }
+}
+function fourth_arrows_rotate(){
+    if(status==0) {
+        $(".more_info:eq(3)").css("display","block");
+        $(".arrow_down:eq(3)").css("transform","rotate(-180deg)");
+        status=1;
+    }
+    else if (status==1) {
+        $(".more_info:eq(3)").css("display","none");
+        $(".arrow_down:eq(3)").css("transform","rotate(-360deg)");
+        status=0;
+    }
+}
+function fifth_arrows_rotate(){
+    if(status==0) {
+        $(".more_info:eq(4)").css("display","block");
+        $(".arrow_down:eq(4)").css("transform","rotate(-180deg)");
+        status=1;
+    }
+    else if (status==1) {
+        $(".more_info:eq(4)").css("display","none");
+        $(".arrow_down:eq(4)").css("transform","rotate(-360deg)");
+        status=0;
+    }
+}
+/*var status = 0;
+function show(num) {
     switch(num) {
         case 1:
         document.getElementsByClassName("hidden how")[0].style.display = "block";
@@ -74,4 +147,4 @@ function fifth_arrows_rotate(num) {
         document.getElementsByClassName("arrow_down")[4].style.transform = "rotate(0deg)";
         status=0;
     }
-}
+}*/
